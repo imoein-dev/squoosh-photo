eea053729e179af576692754e3d53584815961ac
Moein Alipour <moeinalipoor696@gmail.com>
2026-05-19 09:50:41 +0330

    Add output export for Cloudflare Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export', // 🌟 این خط رو حتماً اضافه کن
+  // بقیه کدهایی که از قبل داشتی...
 };
-
 export default nextConfig;
